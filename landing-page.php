f9b92398d70ae34977d5636434b21789f5c57425
Martin Greenwood <martin@pixelpudu.com>
2017-01-20 12:40:01 +0000

    fix

--- landing-page.php
+++ landing-page.php
@@ -69,193 +69,239 @@ get_header(); ?>
 	<div id="primary" class="content-area container">
 		<main id="main" class="site-main row" role="main">
 
-			<div class="row top">
-
-				<div class="column eq-height one">
-				
-					<?php
-					while ( have_posts() ) : the_post();
-
-						get_template_part( 'template-parts/content', 'page-alt' );
-
-					endwhile;
-
-					$featOne = get_field('feature_one');
-					if ($featOne):
-					$featOneID = get_post_thumbnail_id( $featOne->ID );
-					$featOneURL = wp_get_attachment_image_src( $featOneID, 'full' );
-					?>
-					<article class="one" style="background-image: url(<?php echo $featOneURL[0]; ?>);">
-						<a href="<?php echo get_permalink( $featOne->ID ); ?>">
-							<div class="table">
-								<div class="cell bottom">
-									<span><?php echo get_the_title( $featOne->ID ); ?></span>
-									<div class="excerpt">
-									<?php 
-										if (get_field( 'feature_one_intro' )):
-											$excerpt = get_field( 'feature_one_intro' );
-										else:
-											$excerpt = "<p>".get_the_excerpt( $featOne->ID )."</p>"; 
-										endif;
-										echo $excerpt; 
-									?>
-									</div>
-								</div>
-							</div>
-						</a>
-					</article>
-					<?php else: ?>
-					<article class="one" style="background-image: url(//upload.wikimedia.org/wikipedia/commons/2/24/Kendal-Cumbria-4.jpg);">
-					</article>
-					<?php endif; ?>
-
-				</div>
-
-				<div class="column eq-height two">
-					<?php
-					$featTwo = get_field('feature_two');
-					if($featTwo):
-					$featTwoID = get_post_thumbnail_id( $featTwo->ID );
-					$featTwoURL = wp_get_attachment_image_src( $featTwoID, 'full' );
-					?>
-					<article class="two" style="background-image: url(<?php echo $featTwoURL[0]; ?>);">
-						<a href="<?php echo get_permalink( $featTwo->ID ); ?>">
-							<div class="table">
-								<div class="cell bottom">
-									<span><?php echo get_the_title( $featTwo->ID ); ?></span>
-									<div class="excerpt">
-									<?php 
-										if (get_field( 'feature_two_intro' )):
-											$excerpt = get_field( 'feature_two_intro' );
-										else:
-											$excerpt = "<p>".get_the_excerpt( $featTwo->ID )."</p>"; 
-										endif;
-										echo $excerpt; 
-									?>
-									</div>
-								</div>
-							</div>
-						</a>
-					</article>
-					<?php else: ?>
-					<article class="two" style="background-image: url(//upload.wikimedia.org/wikipedia/commons/2/24/Kendal-Cumbria-4.jpg);">
-					</article>
-					<?php endif; ?>
-
-				</div>
-
-			</div>
-
-
-			<div class="row bottom">
-
-				<div class="column one">
-					<?php
-					$featThree = get_field('feature_three');
-					if($featThree):
-					$featThreeID = get_post_thumbnail_id( $featThree->ID );
-					$featThreeURL = wp_get_attachment_image_src( $featThreeID, 'full' );
-					?>
-					<article class="three" style="background-image: url(<?php echo $featThreeURL[0]; ?>);">
-						<a href="<?php echo get_permalink( $featThree->ID ); ?>">
-							<div class="table">
-								<div class="cell bottom">
-									<span><?php echo get_the_title( $featThree->ID ); ?></span>
-									<div class="excerpt">
-									<?php 
-										if (get_field( 'feature_three_intro' )):
-											$excerpt = get_field( 'feature_three_intro' );
-										else:
-											$excerpt = "<p>".get_the_excerpt( $featThree->ID )."</p>"; 
-										endif;
-										echo $excerpt; 
-									?>
-									</div>
-								</div>
-							</div>
-						</a>
-					</article>
-					<?php else: ?>
-					<article class="three" style="background-image: url(//upload.wikimedia.org/wikipedia/commons/2/24/Kendal-Cumbria-4.jpg);">
-					</article>
-					<?php endif; ?>
-
-				</div>
-				
-				<div class="column two">
+			<secton id="discover">
+				<div class="row">
 					
-					<div class="left eq-height">
-						<?php
-						$featFour = get_field('feature_four');
-						if($featFour):
-						$featFourID = get_post_thumbnail_id( $featFour->ID );
-						$featFourURL = wp_get_attachment_image_src( $featFourID, 'full' );
-						?>
-						<article class="four" style="background-image: url(<?php echo $featFourURL[0]; ?>);">
-							<a href="<?php echo get_permalink( $featFour->ID ); ?>">
-								<div class="table">
-									<div class="cell bottom">
-										<span><?php echo get_the_title( $featFour->ID ); ?></span>
-										<div class="excerpt">
-										<?php 
-											if (get_field( 'feature_four_intro' )):
-												$excerpt = get_field( 'feature_four_intro' );
-											else:
-												$excerpt = "<p>".get_the_excerpt( $featFour->ID )."</p>"; 
-											endif;
-											echo $excerpt; 
-										?>
+					<article class="grid">
+
+						<div class="row">
+							<div class="column">
+								<aside class="text">
+									<p><?php the_field( 'discover_intro' ); ?></p>
+								</aside>
+
+								<?php
+								$feature_one = get_field('feature_one');
+								$feature_oneID = get_post_thumbnail_id( $feature_one->ID );
+								$feature_oneURL = wp_get_attachment_image_src( $feature_oneID, 'full' );
+								?>
+								<aside class="image" style="background-image: url(<?php echo $feature_oneURL[0]; ?>);">
+									<a href="<?php echo get_permalink( $feature_one->ID ); ?>">
+										<div class="table">
+											<div class="cell bottom">
+												<span><?php echo get_the_title( $feature_one->ID ); ?></span>
+												<div class="excerpt">
+												<?php 
+													if (get_field( 'feature_one_intro' )):
+														$excerpt = get_field( 'feature_one_intro' );
+													else:
+														$excerpt = "<p>".get_the_excerpt( $feature_one->ID )."</p>"; 
+													endif;
+													echo $excerpt; 
+												?>
+												</div>
+											</div>
 										</div>
-									</div>
-								</div>
-							</a>
-						</article>
-						<?php else: ?>
-						<article class="two" style="background-image: url(//upload.wikimedia.org/wikipedia/commons/2/24/Kendal-Cumbria-4.jpg);">
-						</article>
-						<?php endif; ?>
+									</a>
+								</aside>
 
-					</div>
-					
-					<div class="right eq-height">
-						<?php
-						$featFive = get_field('feature_five');
-						if($featFive):
-						$featFiveID = get_post_thumbnail_id( $featFive->ID );
-						$featFiveURL = wp_get_attachment_image_src( $featFiveID, 'full' );
-						?>
-						<article class="five" style="background-image: url(<?php echo $featFiveURL[0]; ?>);">
-							<a href="<?php echo get_permalink( $featFive->ID ); ?>">
-								<div class="table">
-									<div class="cell bottom">
-										<span><?php echo get_the_title( $featFive->ID ); ?></span>
-										<div class="excerpt">
-										<?php 
-											if (get_field( 'feature_five_intro' )):
-												$excerpt = get_field( 'feature_five_intro' );
-											else:
-												$excerpt = "<p>".get_the_excerpt( $featFive->ID )."</p>"; 
-											endif;
-											echo $excerpt; 
-										?>
+								<?php
+								$feature_two = get_field('feature_two');
+								$feature_twoID = get_post_thumbnail_id( $feature_two->ID );
+								$feature_twoURL = wp_get_attachment_image_src( $feature_twoID, 'full' );
+								?>
+								<aside class="image" style="background-image: url(<?php echo $feature_twoURL[0]; ?>);">
+									<a href="<?php echo get_permalink( $feature_two->ID ); ?>">
+										<div class="table">
+											<div class="cell bottom">
+												<span><?php echo get_the_title( $feature_two->ID ); ?></span>
+												<div class="excerpt">
+												<?php 
+													if (get_field( 'feature_two_intro' )):
+														$excerpt = get_field( 'feature_two_intro' );
+													else:
+														$excerpt = "<p>".get_the_excerpt( $feature_two->ID )."</p>"; 
+													endif;
+													echo $excerpt; 
+												?>
+												</div>
+											</div>
 										</div>
-									</div>
+									</a>
+								</aside>
+
+								<?php
+								$feature_three = get_field('feature_three');
+								$feature_threeID = get_post_thumbnail_id( $feature_three->ID );
+								$feature_threeURL = wp_get_attachment_image_src( $feature_threeID, 'full' );
+								?>
+								<aside class="image" style="background-image: url(<?php echo $feature_threeURL[0]; ?>);">
+									<a href="<?php echo get_permalink( $feature_three->ID ); ?>">
+										<div class="table">
+											<div class="cell bottom">
+												<span><?php echo get_the_title( $feature_three->ID ); ?></span>
+												<div class="excerpt">
+												<?php 
+													if (get_field( 'feature_three_intro' )):
+														$excerpt = get_field( 'feature_three_intro' );
+													else:
+														$excerpt = "<p>".get_the_excerpt( $feature_three->ID )."</p>"; 
+													endif;
+													echo $excerpt; 
+												?>
+												</div>
+											</div>
+										</div>
+									</a>
+								</aside>
+
+							</div>
+							<div class="column">
+								<?php
+								$feature_four = get_field('feature_four');
+								$feature_fourID = get_post_thumbnail_id( $feature_four->ID );
+								$feature_fourURL = wp_get_attachment_image_src( $feature_fourID, 'full' );
+								?>
+								<aside class="image full" style="background-image: url(<?php echo $feature_fourURL[0]; ?>);">
+									<a href="<?php echo get_permalink( $feature_four->ID ); ?>">
+										<div class="table">
+											<div class="cell bottom">
+												<span><?php echo get_the_title( $feature_four->ID ); ?></span>
+												<div class="excerpt">
+												<?php 
+													if (get_field( 'feature_four_intro' )):
+														$excerpt = get_field( 'feature_four_intro' );
+													else:
+														$excerpt = "<p>".get_the_excerpt( $feature_four->ID )."</p>"; 
+													endif;
+													echo $excerpt; 
+												?>
+												</div>
+											</div>
+										</div>
+									</a>
+								</aside>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="column">
+								<?php
+								$feature_five = get_field('feature_five');
+								$feature_fiveID = get_post_thumbnail_id( $feature_five->ID );
+								$feature_fiveURL = wp_get_attachment_image_src( $feature_fiveID, 'full' );
+								?>
+								<aside class="image full" style="background-image: url(<?php echo $feature_fiveURL[0]; ?>);">
+									<a href="<?php echo get_permalink( $feature_five->ID ); ?>">
+										<div class="table">
+											<div class="cell bottom">
+												<span><?php echo get_the_title( $feature_five->ID ); ?></span>
+												<div class="excerpt">
+												<?php 
+													if (get_field( 'feature_five_intro' )):
+														$excerpt = get_field( 'feature_five_intro' );
+													else:
+														$excerpt = "<p>".get_the_excerpt( $feature_five->ID )."</p>"; 
+													endif;
+													echo $excerpt; 
+												?>
+												</div>
+											</div>
+										</div>
+									</a>
+								</aside>
+							</div>
+							<div class="column">
+								<div class="half">
+									<?php
+									$feature_six = get_field('feature_six');
+									$feature_sixID = get_post_thumbnail_id( $feature_six->ID );
+									$feature_sixURL = wp_get_attachment_image_src( $feature_sixID, 'full' );
+									?>
+									<aside class="image" style="background-image: url(<?php echo $feature_sixURL[0]; ?>);">
+										<a href="<?php echo get_permalink( $feature_six->ID ); ?>">
+											<div class="table">
+												<div class="cell bottom">
+													<span><?php echo get_the_title( $feature_six->ID ); ?></span>
+													<div class="excerpt">
+													<?php 
+														if (get_field( 'feature_six_intro' )):
+															$excerpt = get_field( 'feature_two_intro' );
+														else:
+															$excerpt = "<p>".get_the_excerpt( $feature_six§->ID )."</p>"; 
+														endif;
+														echo $excerpt; 
+													?>
+													</div>
+												</div>
+											</div>
+										</a>
+									</aside>
+
+									<?php
+									$feature_seven = get_field('feature_seven');
+									$feature_sevenID = get_post_thumbnail_id( $feature_seven->ID );
+									$feature_sevenURL = wp_get_attachment_image_src( $feature_sevenID, 'full' );
+									?>
+									<aside class="image" style="background-image: url(<?php echo $feature_sevenURL[0]; ?>);">
+										<a href="<?php echo get_permalink( $feature_seven->ID ); ?>">
+											<div class="table">
+												<div class="cell bottom">
+													<span><?php echo get_the_title( $feature_seven->ID ); ?></span>
+													<div class="excerpt">
+													<?php 
+														if (get_field( 'feature_seven_intro' )):
+															$excerpt = get_field( 'feature_seven_intro' );
+														else:
+															$excerpt = "<p>".get_the_excerpt( $feature_seven->ID )."</p>"; 
+														endif;
+														echo $excerpt; 
+													?>
+													</div>
+												</div>
+											</div>
+										</a>
+									</aside>
 								</div>
-							</a>
-						</article>
-						<?php else: ?>
-						<article class="two" style="background-image: url(//upload.wikimedia.org/wikipedia/commons/2/24/Kendal-Cumbria-4.jpg);">
-						</article>
-						<?php endif; ?>
+						
+								<div class="half">
+									<?php
+									$feature_eight = get_field('feature_eight');
+									$feature_eightID = get_post_thumbnail_id( $feature_eight->ID );
+									$feature_eightURL = wp_get_attachment_image_src( $feature_eightID, 'full' );
+									?>
+									<aside class="image" style="background-image: url(<?php echo $feature_eightURL[0]; ?>);">
+										<a href="<?php echo get_permalink( $feature_eight->ID ); ?>">
+											<div class="table">
+												<div class="cell bottom">
+													<span><?php echo get_the_title( $feature_eight->ID ); ?></span>
+													<div class="excerpt">
+													<?php 
+														if (get_field( 'feature_eight_intro' )):
+															$excerpt = get_field( 'feature_eight_intro' );
+														else:
+															$excerpt = "<p>".get_the_excerpt( $feature_eight->ID )."</p>"; 
+														endif;
+														echo $excerpt; 
+													?>
+													</div>
+												</div>
+											</div>
+										</a>
+									</aside>
 
-						<article class="six">
-							<?php echo get_field('feature_text'); ?>
-						</article>
-					</div>
+									
+									<aside class="text">
+										<?php the_field('feature_text'); ?>
+									</aside>
+								</div>
+							</div>
+						</div>
 
-				</div>				
-
-			</div>
+						
+					</article>
+				</div>
+			</secton>
 
 		</main>
 	</div>
